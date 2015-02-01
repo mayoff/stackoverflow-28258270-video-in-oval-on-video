53e0a0bf151eccd26aa3a6846cc6c9e55188cae4
Rob Mayoff <git@rob.dqd.com>
2015-02-01 01:34:50 -0600

    working version using custom compositor and Core Graphics

--- video/CustomVideoCompositor.m
+++ video/CustomVideoCompositor.m
@@ -7,6 +7,7 @@
 //
 
 #import "CustomVideoCompositor.h"
+@import  UIKit;
 
 @implementation CustomVideoCompositor
 
@@ -15,14 +16,20 @@
 }
 
 - (void)startVideoCompositionRequest:(AVAsynchronousVideoCompositionRequest *)request {
-    CVPixelBufferRef buffer = [request sourceFrameByTrackID:[request.sourceTrackIDs.firstObject intValue]];
-    if (buffer) {
-        CVBufferRetain(buffer);
-    } else {
-        buffer = [request.renderContext newPixelBuffer];
+    CVPixelBufferRef destination = [request.renderContext newPixelBuffer];
+    if (request.sourceTrackIDs.count == 2) {
+        CVPixelBufferRef front = [request sourceFrameByTrackID:1];
+        CVPixelBufferRef back = [request sourceFrameByTrackID:2];
+        CVPixelBufferLockBaseAddress(front, kCVPixelBufferLock_ReadOnly);
+        CVPixelBufferLockBaseAddress(back, kCVPixelBufferLock_ReadOnly);
+        CVPixelBufferLockBaseAddress(destination, 0);
+        [self renderFrontBuffer:front backBuffer:back toBuffer:destination];
+        CVPixelBufferUnlockBaseAddress(destination, 0);
+        CVPixelBufferUnlockBaseAddress(back, kCVPixelBufferLock_ReadOnly);
+        CVPixelBufferUnlockBaseAddress(front, kCVPixelBufferLock_ReadOnly);
     }
-    [request finishWithComposedVideoFrame:buffer];
-    CVBufferRelease(buffer);
+    [request finishWithComposedVideoFrame:destination];
+    CVBufferRelease(destination);
 }
 
 - (void)renderContextChanged:(AVVideoCompositionRenderContext *)newRenderContext {
@@ -34,6 +41,34 @@
 
 - (NSDictionary *)sourcePixelBufferAttributes {
     return @{ (__bridge NSString *)kCVPixelBufferPixelFormatTypeKey : @[ @(kCVPixelFormatType_32BGRA) ] };
+}
+
+- (void)renderFrontBuffer:(CVPixelBufferRef)front backBuffer:(CVPixelBufferRef)back toBuffer:(CVPixelBufferRef)destination {
+    CGImageRef frontImage = [self createSourceImageFromBuffer:front];
+    CGImageRef backImage = [self createSourceImageFromBuffer:back];
+    size_t width = CVPixelBufferGetWidth(destination);
+    size_t height = CVPixelBufferGetHeight(destination);
+    CGRect frame = CGRectMake(0, 0, width, height);
+    CGContextRef gc = CGBitmapContextCreate(CVPixelBufferGetBaseAddress(destination), width, height, 8, CVPixelBufferGetBytesPerRow(destination), CGImageGetColorSpace(backImage), CGImageGetBitmapInfo(backImage));
+    CGContextDrawImage(gc, frame, backImage);
+    CGContextBeginPath(gc);
+    CGContextAddEllipseInRect(gc, CGRectInset(frame, frame.size.width / 10, frame.size.height / 10));
+    CGContextClip(gc);
+    CGContextDrawImage(gc, frame, frontImage);
+    CGContextRelease(gc);
+}
+
+- (CGImageRef)createSourceImageFromBuffer:(CVPixelBufferRef)buffer {
+    size_t width = CVPixelBufferGetWidth(buffer);
+    size_t height = CVPixelBufferGetHeight(buffer);
+    size_t stride = CVPixelBufferGetBytesPerRow(buffer);
+    void *data = CVPixelBufferGetBaseAddress(buffer);
+    CGColorSpaceRef rgb = CGColorSpaceCreateDeviceRGB();
+    CGDataProviderRef provider = CGDataProviderCreateWithData(NULL, data, height * stride, NULL);
+    CGImageRef image = CGImageCreate(width, height, 8, 32, stride, rgb, kCGBitmapByteOrder32Big | kCGImageAlphaPremultipliedLast, provider, NULL, NO, kCGRenderingIntentDefault);
+    CGDataProviderRelease(provider);
+    CGColorSpaceRelease(rgb);
+    return image;
 }
 
 @end
