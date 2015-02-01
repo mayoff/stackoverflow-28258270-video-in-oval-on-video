61dde9b1d1ac5a74299fd5a73b5813a8872eba12
Rob Mayoff <git@rob.dqd.com>
2015-01-31 20:31:00 -0600

    Initial Commit

---
+++ video/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  video
+//
+//  Created by Rob Mayoff on 1/31/15.
+//  Copyright (c) 2015 Rob Mayoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ video/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  video
+//
+//  Created by Rob Mayoff on 1/31/15.
+//  Copyright (c) 2015 Rob Mayoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
